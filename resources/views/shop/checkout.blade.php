c226f9677f682da452589a38676c2c4617c9a792
JABER-AHMED <placidcse@gmail.com>
2017-07-22 17:56:53 +0600

    nothing so changed

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -11,6 +11,9 @@
 		<div class="col-sm-6 col-md-4 col-md-offset-4 col-sm-offset-3">
 			<h1>Checkout</h1>
 			<h4>Your Total: ${{ $total }}</h4>
+			<div id="charge-error" class="alert alert-danger {{ !Session::has('error') ? 'hidden' : '' }}">{{ Session::get('error') }}
+				
+			</div>
 			<form action="{{ route('checkout') }}" method="post" id="checkout-form">
 				<div class="row">
 					<div class="col-xs-12">
@@ -66,4 +69,10 @@
 		</div>
 	</div>
 
+@endsection
+
+@section('scripts')
+
+	<script type="text/javascript" src="https://js.stripe.com/v3/"></script>
+	<script type="text/javascript" src="{{asset('src/js/checkout.js')}}"></script>
 @endsection
